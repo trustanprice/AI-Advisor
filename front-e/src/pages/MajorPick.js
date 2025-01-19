f00bbaa31eb4122cfdf9307f6e7af6a378dd24b1
christopherwilliams <wchristopherj.2@gmial.com>
2025-01-18 22:58:52 -0600

    Initial commit

---
+++ front-e/src/pages/MajorPick.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MajorPick() {
+    return (
+        <div>
+            <h1>Change Your Major</h1>
+            <p>Explore new majors and update your academic focus.</p>
+        </div>
+    );
+}
+
+export default MajorPick;
